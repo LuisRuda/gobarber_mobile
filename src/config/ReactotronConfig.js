7c8e57b4f4d2d4068955263da264b04bd00fb5ef
Luis Paulo Ruda <luispauloruda@gmail.com>
2019-07-29 19:50:54 -0300

    Reactotron

---
+++ src/config/ReactotronConfig.js
@@ -0,0 +1,15 @@
+import Reactotron from 'reactotron-react-native';
+import { reactotronRedux } from 'reactotron-redux';
+import reactrotronSaga from 'reactotron-redux-saga';
+
+if (__DEV__) {
+  const tron = Reactotron.configure({ host: '10.0.0.104' })
+    .useReactNative()
+    .use(reactotronRedux())
+    .use(reactrotronSaga())
+    .connect();
+
+  tron.clear();
+
+  console.tron = tron;
+}
