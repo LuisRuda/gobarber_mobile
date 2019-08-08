c60b12250be6604096759956b7325743a96dfd84
Luis Paulo Ruda <luispauloruda@gmail.com>
2019-08-08 20:40:37 -0300

    Estilizações das rotas

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
-import { View } from 'react-native';
+import NavigationBar from 'react-native-navbar-color';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import Background from '~/components/Background';
 
 // import { Container } from './styles';
 
 export default function Dashboard() {
-  return <View />;
+  NavigationBar.setColor('#8d41a8');
+  NavigationBar.setStatusBarTheme('light', true);
+
+  return <Background />;
 }
+
+Dashboard.navigationOptions = {
+  tabBarLabel: 'Agendamentos',
+  tabBarIcon: ({ tintColor }) => (
+    <Icon name="event" size={20} color={tintColor} />
+  ),
+};
